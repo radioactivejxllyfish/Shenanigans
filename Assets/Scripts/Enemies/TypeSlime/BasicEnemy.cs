3e6fe75ba99c5d39c9ecd1865781171a2eb74f96
ESCAPEPOD <vuvietnam1528@gmail.com>
2025-02-15 17:53:10 +0700

    0.0.3d +HP bar module +New flip system +New fonts +Camera shake +Reworked player sprites & animations +Added Dashing

---
+++ Assets/Scripts/Enemies/TypeSlime/BasicEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    public float health;
+    public float maxhealth;
+    public float speed;
+    public float damage;
+
+    public bool isAlive;
+    public bool isStunned;
+    
+    public Vector2 direction;
+
+    public Animator animator;
+    public GameObject player;
+    public Rigidbody2D rigidBody;
+    public CircleCollider2D sight;
+    public SpriteRenderer spriteRenderer;
+
+    
+    
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        speed -= speed * (5f/100f) ;
+    }
+
+}
